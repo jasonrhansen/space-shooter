71508c97eddd94cd5a736862e19fb1d343afa0b2
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2024-01-20 13:26:31 -0700

    Don't print score to the console

--- src/score/mod.rs
+++ src/score/mod.rs
@@ -12,9 +12,6 @@ impl Plugin for ScorePlugin {
     fn build(&self, app: &mut App) {
         app.init_resource::<Score>()
             .init_resource::<HighScores>()
-            .add_systems(
-                Update,
-                (update_score, update_high_scores, high_scores_updated).chain(),
-            );
+            .add_systems(Update, (update_high_scores, high_scores_updated).chain());
     }
 }
--- src/score/systems.rs
+++ src/score/systems.rs
@@ -3,12 +3,6 @@ use bevy::prelude::*;
 use super::resources::*;
 use crate::events::*;
 
-pub fn update_score(score: Res<Score>) {
-    if score.is_changed() {
-        println!("Score: {}", score.value);
-    }
-}
-
 pub fn update_high_scores(
     mut game_over_reader: EventReader<GameOver>,
     mut high_scores: ResMut<HighScores>,
