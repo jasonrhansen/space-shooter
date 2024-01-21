4ae0b1a52396387f39bd014bd3e5ed6a91db17af
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2024-01-21 10:50:16 -0700

    Add better styling/layout to paused screen

--- src/osd/mod.rs
+++ src/osd/mod.rs
@@ -13,7 +13,7 @@ impl Plugin for OsdPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, setup)
             .add_systems(Update, update_score_text)
-            .add_systems(OnEnter(AppState::Paused), show_paused_screen)
-            .add_systems(OnExit(AppState::Paused), remove_paused_screen);
+            .add_systems(OnEnter(AppState::Paused), spawn_paused_screen)
+            .add_systems(OnExit(AppState::Paused), despawn_paused_screen);
     }
 }
--- src/osd/systems.rs
+++ src/osd/systems.rs
@@ -32,22 +32,35 @@ pub fn update_score_text(score: Res<Score>, mut query: Query<&mut Text, With<Sco
     }
 }
 
-pub fn show_paused_screen(mut commands: Commands) {
-    commands.spawn((
-        TextBundle::from_section(
-            "Paused",
-            TextStyle {
-                font_size: 200.0,
+pub fn spawn_paused_screen(mut commands: Commands) {
+    commands
+        .spawn((
+            NodeBundle {
+                style: Style {
+                    position_type: PositionType::Absolute,
+                    width: Val::Percent(100.0),
+                    height: Val::Percent(100.0),
+                    justify_content: JustifyContent::Center,
+                    align_items: AlignItems::Center,
+                    ..default()
+                },
+                background_color: Color::rgba(0.0, 0.0, 0.0, 0.5).into(),
                 ..default()
             },
-        )
-        .with_text_alignment(TextAlignment::Center)
-        .with_style(Style { ..default() }),
-        PausedText,
-    ));
+            PausedText,
+        ))
+        .with_children(|parent| {
+            parent.spawn(TextBundle::from_section(
+                "Paused",
+                TextStyle {
+                    font_size: 200.0,
+                    ..default()
+                },
+            ));
+        });
 }
 
-pub fn remove_paused_screen(mut commands: Commands, query: Query<Entity, With<PausedText>>) {
+pub fn despawn_paused_screen(mut commands: Commands, query: Query<Entity, With<PausedText>>) {
     if let Ok(entity) = query.get_single() {
         commands.entity(entity).despawn_recursive();
     }
